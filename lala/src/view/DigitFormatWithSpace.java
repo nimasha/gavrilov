229f24572900935498b0a54effe45980d3b5d669
nimasha <nimasha@mail.ru>
2015-01-07 19:20:35 +0400

    tatata

---
+++ lala/src/view/DigitFormatWithSpace.java
@@ -0,0 +1,30 @@
+package view;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class DigitFormatWithSpace implements KeyListener {
+
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		char c = e.getKeyChar();
+		if (!(Character.isDigit(c) || (c == KeyEvent.VK_BACK_SPACE) || (c == KeyEvent.VK_SPACE) || (c == KeyEvent.VK_DELETE))) {
+			e.consume();
+		}
+
+	}
+}
